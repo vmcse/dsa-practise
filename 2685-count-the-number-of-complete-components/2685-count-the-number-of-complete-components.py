38b14596c5cd52411032b74d05a1fc1efcd83650
PITTA VENKATA VAMSI MADHAV <vmcse09@gmail.com>
2026-07-11 10:48:04 +0530

    Time: 47 ms (48.76%), Space: 19.7 MB (90.53%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -1,24 +1,35 @@
 class Solution:
     def countCompleteComponents(self, n: int, edges: list[list[int]]) -> int:
-        root = list(range(n))
-
-        def find(i):
-            root[i] = find(root[i]) if root[i] != i else i
-            return root[i]
-
+        graph = defaultdict(set)
         for u, v in edges:
-            root[find(u)] = find(v)
-
-        V, E = [[0] * n for _ in range(2)]
-
-        for i in range(n):
-            V[find(i)] += 1
-            
-        for u, _ in edges:
-            E[find(u)] += 1
-
+            graph[u].add(v)
+            graph[v].add(u)
+        
+        visited = [False] * n
         res = 0
         for i in range(n):
-            res += V[i] and E[i] == V[i] * (V[i] - 1) // 2
+            if visited[i]:
+                continue
+            
+            visited[i] = True
+            q = deque([i])
+            component = []
+
+            while q:
+                u = q.pop()
+                component.append(u)
+
+                for v in graph[u]:
+                    if not visited[v]:
+                        q.append(v)
+                        visited[v] = True
+            
+            k = len(component)
+            res += 1
+
+            for u in component:
+                if len(graph[u]) != k - 1:
+                    res -= 1
+                    break
 
         return res
